ec2fa3504a55d0646da762b4d20d0219b745a688
VinayBorate <vinayborate121@gmail.com>
2025-01-10 20:38:32 +0530

    successfull

--- README.md
+++
@@ -1,1 +0,0 @@
-# KoinX_Assignment
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Headerbar from "./components/Headerbar"
+import Criptopage from "./pages/Criptopage"
+
+function App() {
+  return (
+    <div>
+       <Headerbar/>
+       <Criptopage/>
+    </div>
+  )
+}
+
+export default App
